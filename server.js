c0b01272eb5b754a2c4b6b14749ba8d5037eb35a
premarajanmk <premarajanmk@gmail.com>
2016-09-19 14:48:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function(req,res){res, send('article one requested and will be served here');
+app.get('/article-one', function(req,res){res.send('article one requested and will be served here');
 });
 
-app.get('/article-two', function(req,res){res, send('article one requested and will be served here');
+app.get('/article-two', function(req,res){res.send('article one requested and will be served here');
 });
-app.get('/article-three', function(req,res){res, send('article one requested and will be served here');
+app.get('/article-three', function(req,res){res.send('article one requested and will be served here');
 });
 
 
