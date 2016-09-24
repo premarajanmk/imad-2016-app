c4160bcfbea142b669fefdcc4d3eba5aa17ea35b
premarajanmk <premarajanmk@gmail.com>
2016-09-24 14:07:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element= document.getElementById('main-text');
 element.innerHTML = 'New Value';
 //move the image
 var img = document.getElementById('madi');
+marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.margin = marginLeft = 'px';
+}
 img.onclick =function(){
+    var  interval = setInterval (moveRight,100);
     img.style.marginLeft = '100px';
 };
